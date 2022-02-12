07bd156527b702d952aa6f583c37a9b0cd183626
Fabio Mattes <fabio.mattes@benner.com.br>
2022-02-12 18:34:18 -0300

    Criação da tabela seguidores

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -9,3 +9,10 @@ CREATE TABLE usuarios(
     senha varchar(150) NOT NULL,
     criadoEm timestamp DEFAULT CURRENT_TIMESTAMP()
 ) ENGINE = INNODB;
+CREATE TABLE seguidores(
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    seguidor_id INT NOT NULL,
+    FOREIGN KEY (seguidor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    PRIMARY KEY(usuario_id, seguidor_id)
+) ENGINE = INNODB;
